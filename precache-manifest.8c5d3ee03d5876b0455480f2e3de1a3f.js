a92a0e8d7de5dce52401780743651a42a718b91a
pardhasaradhichodey <pardhu.dhoni@gmail.com>
2022-01-24 12:42:51 +0530

    Updates

---
+++ precache-manifest.8c5d3ee03d5876b0455480f2e3de1a3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec0d305d2a54019a788ba972a4210fa",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a6570f26a0136cddd4ed",
+    "url": "/monsters-rolodex/static/css/main.3541833c.chunk.css"
+  },
+  {
+    "revision": "c8e56e1d6cf27670d4ed",
+    "url": "/monsters-rolodex/static/js/2.580db2ef.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/monsters-rolodex/static/js/2.580db2ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "929a097e7e5f9dac4ac5",
+    "url": "/monsters-rolodex/static/js/3.f65c981b.chunk.js"
+  },
+  {
+    "revision": "a6570f26a0136cddd4ed",
+    "url": "/monsters-rolodex/static/js/main.e12f0236.chunk.js"
+  },
+  {
+    "revision": "f75b4157cb8ee99d8319",
+    "url": "/monsters-rolodex/static/js/runtime-main.5946d3b9.js"
+  }
+]);
