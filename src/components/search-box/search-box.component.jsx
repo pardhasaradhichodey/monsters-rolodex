1db5f28579ec0d63422f17428c5138f8933a42ab
pardhasaradhichodey <pardhu.dhoni@gmail.com>
2022-01-24 12:30:39 +0530

    first commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './search-box.styles.css';
+export const SearchBox = ({ placeholder, handleChange }) => (
+    <input 
+        className="search"
+        type='search' 
+        placeholder={placeholder} 
+        onChange={handleChange}
+    />
+);
